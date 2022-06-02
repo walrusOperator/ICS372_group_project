4ded5b5364a5bc44d25f455b393aacad7fdac6f7
KiyokoNee <k.c.gearing@gmail.com>
2022-06-02 09:26:58 -0500

    first draft of readJSON()

--- src/Utilities.java
+++ src/Utilities.java
@@ -1,2 +1,20 @@
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 public class Utilities {
+
+    // reads in a JSON file and converts into a JSON object
+    public JSONObject readJSON(){
+        JSONParser parser = new JSONParser();
+
+        try {
+            Object obj = parser.parse(new FileReader("whatever the file ends up being"));
+            return (JSONObject) obj;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
