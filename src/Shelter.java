f176295361f6fb8e5f0fc9dbbe9c4b468d8c5552
NickSigwanz <101123809+NickSigwanz@users.noreply.github.com>
2022-06-03 09:37:30 -0500

    initial implementation of  shelter class

--- src/Shelter.java
+++ src/Shelter.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shelter {
+    private List<Animal> animalList = new ArrayList<Animal>();
+    private String shelterID;
+    private  Boolean receiving;
+    Shelter(String shelterId){
+        this.shelterID = shelterId;
+    }
+    public Shelter createShelter(){
+        return new Shelter();
+    }
+    public boolean enableReceiving(){
+        this.receiving = true;
+        return this.receiving;
+    }
+    public boolean disableReceiving (){
+        this.receiving = false;
+        return this.receiving;
+    }
 }
