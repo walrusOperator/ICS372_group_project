3b4c2ee2fcd9e7cbf70dcbf48c7a254f53004265
walrusOperator <indierevolver@gmail.com>
2022-06-06 16:45:52 -0500

    Started main and modified shelter list

--- src/main/java/ShelterList.java
+++ src/main/java/ShelterList.java
@@ -1,38 +1,21 @@
+import org.json.simple.*;
 import org.json.simple.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Scanner;
 
 public class ShelterList {
+//    public static void main(String[] args) {
+//
+//    }
 
-    public static void main(String[] args) {
-        List shelters = new ArrayList<JSONObject>();
-        Scanner scan = new Scanner(System.in);
+    public static JSONArray addIncoming() {
+        return Utilities.readJSON();
 
-        System.out.println("Please choose from the following options:\n" +
-                "1: add incoming animals\n" +
-                "2: enable receiving animals\n" +
-                "3: disable receiving animals\n" +
-                "4: export json file\n");
-        int userOption = scan.nextInt();
-        switch (userOption){
-            case 1:
-                Utilities.readJSON();
-//                TODO: provide feedback to user if successful
-                break;
-            case 2:
-//                TODO: list shelters from shelter class to console.
-//                TODO: have user select shelter
-                break;
-            case 3:
-//                TODO: list shelters from shelter class to console.
-//                TODO: have user select shelter
-                break;
-            case 4:
-                Utilities.writeJSON();
-//                TODO: provide user feedback
-                break;
-        }
+//        TODO: Get JSON array from utilities
+//        TODO: for each loop passing shelter ID to createShelter()
+//        TODO: for each loop of shelter objects to populate with matching animals
+//        TODO: method to return shelters by ShelterID
     }
+
 }
