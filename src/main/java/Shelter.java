e6dbcfdabbf9681167afdf9bdec2c526a8d77de3
NickSigwanz <101123809+NickSigwanz@users.noreply.github.com>
2022-06-06 15:34:29 -0500

    Update Shelter.java

--- src/main/java/Shelter.java
+++ src/main/java/Shelter.java
@@ -5,18 +5,21 @@ public class Shelter {
     private List<Animal> animalList = new ArrayList<Animal>();
     private String shelterID;
     private  Boolean receiving;
+
     Shelter(String shelterId){
         this.shelterID = shelterId;
     }
-    public Shelter createShelter(){
-        return new Shelter();
+
+    public Shelter createShelter(String id){
+        return new Shelter(id);
+
     }
-    public boolean enableReceiving(){
+    public void enableReceiving(){
         this.receiving = true;
-        return this.receiving;
+
     }
-    public boolean disableReceiving (){
+    public void disableReceiving (){
         this.receiving = false;
-        return this.receiving;
+
     }
 }
