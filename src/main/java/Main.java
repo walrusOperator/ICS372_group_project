3993fe033ab4d4683b68abd1de57adbec325bec9
walrusOperator <indierevolver@gmail.com>
2022-06-07 18:43:49 -0500

    created hashmap structure in ShelterList class. May transfer tro stand alone class at later date.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,25 +8,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List shelters = new ArrayList<JSONObject>();
+        ShelterList shelterList = new ShelterList();
+//        List shelters = new ArrayList<JSONObject>();
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Please choose from the following options:\n" +
-                "1: add incoming animals\n" +
+                "1: Show current animals\n" +
+                "2: add incoming animals\n" +
                 "2: enable receiving animals\n" +
                 "3: disable receiving animals\n" +
                 "4: export json file\n");
         int userOption = scan.nextInt();
         switch (userOption){
             case 1:
-
-                ShelterList.addIncoming();
-//                j.forEach( animal -> parseAnimalObject( (JSONObject) animal));
-
-//                JSONArray jsonArray = (JSONArray) j.get("shelter_roster");
-//                for(Object x : j) {
-//                    System.out.println(j);
-//                }
 //                TODO: provide feedback to user if successful
                 break;
             case 2:
