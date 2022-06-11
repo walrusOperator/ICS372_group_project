e1556de79562e214c1dc7306da9b14c35dbbd660
walrusOperator <indierevolver@gmail.com>
2022-06-11 08:33:54 -0500

    removed repeating code fom shelterExists and addNewAnimal classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -32,9 +32,9 @@ public class Main {
                     shelterMap.showShelters();
                     System.out.println("Please select a shelter: ");
                     selected = scan.nextLine();
-                    shelterSearch(selected);
+                    boolean shelterAvailable = shelterSearch(selected);
 //                TODO: have user add in animal data
-                    if(shelterMap.getShelter(selected).isReceiving()) {
+                    if(shelterAvailable) {
                         addNewAnimal(selected);
                     }
                     break;
@@ -58,14 +58,18 @@ public class Main {
             }
         }
 
-        public static void shelterSearch(String selected){
+        public static boolean shelterSearch(String selected){
             if (shelterMap.containsShelter(selected)) {
                 if (shelterMap.getShelter(selected).isReceiving()) {
-
+                    return true;
                 } else {
-                    System.out.println("This shelter is not receiving animals.");
+                    System.out.println("This shelter is not receiving animals\n");
+                    return false;
                 }
+            } else {
+                System.out.println("Please select a valid shelter\n");
             }
+            return false;
         }
 
         public static void shelterExists(String selected, boolean status) {
@@ -74,47 +78,47 @@ public class Main {
             } else {
                 shelterMap.getShelter(selected).setReceiving(status);
             }
-            if(status == true){
+            if(status){
                 System.out.println("Receiving enabled for shelter " + selected + "\n");
             } else {
                 System.out.println("Receiving disabled for shelter " + selected + "\n");
             }
         }
 
-        public static void addNewAnimal(String selected){
+        public static void addNewAnimal(String selected) {
             Scanner scan = new Scanner(System.in);
-            if(!shelterMap.containsShelter(selected)){
-                System.out.println("This is not a valid shelter.");
+//            if(!shelterMap.containsShelter(selected)){
+//                System.out.println("This is not a valid shelter.");
+//            } else {
+            System.out.println("Please enter the animal type: ");
+            String type = scan.nextLine();
+            if (type.equalsIgnoreCase("cat") ||
+                    type.equalsIgnoreCase("dog") ||
+                    type.equalsIgnoreCase("bird") ||
+                    type.equalsIgnoreCase("rabbit")) {
+
+                System.out.println("Please enter the animal name: ");
+                String name = scan.nextLine();
+                System.out.println("Please enter the animal ID: ");
+                String id = scan.nextLine();
+                System.out.println("Please enter the animal weight: ");
+                double weight = scan.nextDouble();
+                scan.nextLine();
+                System.out.println("Please enter the receipt date: ");
+                long receipt = scan.nextLong();
+                scan.nextLine();
+
+                Animal addition = new Animal(type, name, id, weight, receipt);
+                Shelter tempShelter = shelterMap.getShelter(selected);
+                List<Animal> tempList = tempShelter.getAnimalList();
+                tempList.add(addition);
+                tempShelter.setAnimalList(tempList);
+
+                System.out.println("New Animal has been added.");
             } else {
-                System.out.println("Please enter the animal type: ");
-                String type = scan.nextLine();
-                if(type.equalsIgnoreCase("cat")||
-                        type.equalsIgnoreCase("dog")||
-                        type.equalsIgnoreCase("bird")||
-                        type.equalsIgnoreCase("rabbit")) {
-
-                    System.out.println("Please enter the animal name: ");
-                    String name = scan.nextLine();
-                    System.out.println("Please enter the animal ID: ");
-                    String id = scan.nextLine();
-                    System.out.println("Please enter the animal weight: ");
-                    double weight = scan.nextDouble();
-                    scan.nextLine();
-                    System.out.println("Please enter the receipt date: ");
-                    long receipt = scan.nextLong();
-                    scan.nextLine();
-
-                    Animal addition = new Animal(type, name, id, weight, receipt);
-                    Shelter tempShelter = shelterMap.getShelter(selected);
-                    List<Animal> tempList = tempShelter.getAnimalList();
-                    tempList.add(addition);
-                    tempShelter.setAnimalList(tempList);
-
-                    System.out.println("New Animal has been added.");
-                } else {
-                    System.out.println("Not a valid animal type.");
-                }
+                System.out.println("Not a valid animal type.");
             }
+//        }
         }
 }
 
